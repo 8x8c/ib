66d8871c19b367887e4b9baa7beedcbfde6de6e8
8x8c <x1a7@pm.me>
2025-03-30 18:12:25 -0700

    Add files via upload

---
+++ version3/config.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+// Database credentials for MySQL (XAMPP)
+$dsn    = 'mysql:host=localhost;dbname=articles_db;charset=utf8mb4';
+$dbUser = 'root';
+$dbPass = ''; // Adjust as needed
